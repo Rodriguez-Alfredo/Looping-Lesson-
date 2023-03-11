de2af04bdad2242864e0922d5bcee10bcef53b41
AlfredoRodriguez <126662592+Rodriguez-Alfredo@users.noreply.github.com>
2023-03-11 16:26:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+#LOOPING
+
+#For loops - start to finish of an iterate
+vegetables = ["cucumber", "spinach", "cabbage"]
+
+for x in vegetables:
+	print(x)
+
+
+#example of a ping sweep
+#192.168.1.1-254 
+#ip = 1..254
+#for x in ip 
+#	ping 192.168.1.x
+
+#While loops - execute as long as TRUE
+i = 1
+while i < 10:
+	print(i)
+	i += 1
